e361cc149c8d57ed480115ba03855a8c33844e5b
Cate <caterinailariopaz@alumnos.tuniversformacion.es>
2023-12-04 08:52:36 +0100

    Car class completed

--- scripts/Car.js
+++ scripts/Car.js
@@ -27,15 +27,61 @@ export class Car {
     }
 
     speedUp(speed){
-        const message = `El vehículo ha acelerado hasta ${speed}`
+        const message = `El vehículo ha acelerado hasta ${speed} <br/>`
 
         showResult(message);
     }
 
     start() {
-        const message = `El vehículo con marca ${this.#brand} ha arrancado.`
+        const message = `El vehículo con marca ${this.#brand}, ha arrancado. <br/>`
   
         showResult(message)
+    }
+
+    slowDown(){
+        const message = `El vehículo con marca ${this.#brand}, ha frenado. <br/>`;
+    }
+
+    // Getters y Setters
+
+    get brand() {
+        return this.#brand;
+    }
+
+    set brand(value){
+        this.#brand = value;
+    }
+
+    get model() {
+        return this.#model;
+    }
+
+    set model(value){
+        this.#model = value;
+    }
+
+    get color() {
+        return this.#color;
+    }
+
+    set color(value){
+        this.#color = value;
+    }
+
+    get creation() {
+        return this.#creation;
+    }
+
+    set creation(value){
+        this.#creation = value;
+    }
+
+    get displacement() {
+        return this.#displacement;
+    }
+
+    set displacement(value){
+        this.#displacement = value;
     }
 
 }
@@ -73,102 +119,4 @@ export class Car {
 
 
 
-
-
-
-
-
-
-
-
-// Clase Padre
-export class Padre {
-        #brand;
-        #model;
-        #color;
-        #creation;
-        #displacement;
     
-        // Constructor con parámetros
-        constructor(brand, model, color, creation, displacement){
-            // Encapsulamos el atributo
-            this.#brand = brand;
-            this.#model = model;
-            this.#color = color;
-            this.#creation = creation;
-            this.#displacement = displacement;
-        }
-    
-        // MÉTODOS
-        showDetails(){
-            const data = `
-                La marca es ${this.#brand} <br/>
-                El modelo es ${this.#model} <br/>
-                Su color es ${this.#color} <br/>
-                Se fabricó en el año ${this.#creation} <br/>
-                Cilindrada de ${this.#displacement} 
-            `;
-    
-            // Llamar a la función showResult definida en el main.js. No es un método propio de cada clase
-            // Esta función si estuviéramos con back-end no estaría aqui
-            showResult(data);
-        }
-    
-        speedUp(speed){
-            const message = `<br/> Se ha acelerado hasta ${speed}`;
-            showResult(`<p>${message}</p>`);
-        }
-    
-        start(){
-            const message = `<br/>El coche ${this.#brand}, ${this.#model}, de color ${this.color}, ha arrancado.`;
-            showResult(`<p>${message}</p>`);
-        }
-    
-        slowDown(){
-            const message = `<br/>El coche ${this.#brand}, ${this.#model}, de color ${this.color}, ha parado.`;
-            showResult(`<p>${message}</p>`);
-        }
-    
-        // GETTER Y SETTER
-        get brand(){
-            return this.#brand;
-        }
-    
-        set brand(value){
-            this.#brand = value;
-        }
-    
-        get model(){
-            return this.#model;
-        }
-    
-        set model(value){
-            this.#model = value;
-        }
-    
-        get color(){
-            return this.#color;
-        }
-    
-        set color(value){
-            this.#color = value;
-        }
-    
-        get creation(){
-            return this.#creation;
-        }
-    
-        set creation(value){
-            this.#creation = value;
-        }
-    
-        get displacement(){
-            return this.#displacement;
-        }
-    
-        set displacement(value){
-            this.#displacement = value;
-        }
-
-
-}
